630c97f6cf6a33d84e4792498a8e9085aa6f3a37
Ankar <caarevalo@unal.edu.co>
2022-06-07 13:20:29 -0500

    code

---
+++ corazon.py
@@ -0,0 +1,26 @@
+import turtle
+import time
+
+# Movimiento de curva
+def curveMove():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+
+def drawHeart():
+    turtle.speed(150) # La velocidad del cepillo se ajusta al máximo
+    turtle.color('red','pink')
+    turtle.begin_fill()
+    turtle.left(140) # Gire 140 grados en sentido antihorario
+    turtle.forward(111.65) # Avanzar 111,65 píxeles
+    curveMove() # Dibuja una curva
+    turtle.left(120) # Gire 120 grados en sentido antihorario
+    curveMove() # Continuar dibujando la curva
+    turtle.forward(111.65) # Avanzar 111,65 píxeles
+    turtle.end_fill()
+    time.sleep(10)
+
+if __name__ == '__main__':
+    drawHeart()
+
